bd58861dc634eec49b095aa6fe161e17ed01f41c
Anjanikumar Guruvali <108091230+Anjanikumar-G@users.noreply.github.com>
2024-05-22 12:33:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  try {
+    let currentContent = await fs.readFile(fileName, 'utf-8');
+    currentContent += fileContent;
+    await fs.writeFile(fileName, currentContent);
+    return currentContent;
+  } catch (error) {
+    console.error('Error updating the file:', error);
+    throw error;
+  }
   
 };
 
